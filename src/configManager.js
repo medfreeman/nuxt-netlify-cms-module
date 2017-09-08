fdda1c66c63e1f81c7b64778a3757b73e1cb8d23
Mehdi Lahlou <mlahlou@protonmail.ch>
2017-09-08 15:11:04 +0200

    chore(core): remove always valid test
    Increase coverage

--- src/configManager.js
+++ src/configManager.js
@@ -69,9 +69,7 @@ class ConfigManager {
       collections: []
     };
 
-    if (configObject.media_folder) {
-      newConfig.media_folder = join(enforcedPath, configObject.media_folder);
-    }
+    newConfig.media_folder = join(enforcedPath, configObject.media_folder);
 
     if (configObject.collections) {
       configObject.collections.forEach(function(collection) {

56eff4820786d917f170f3da096623c9deef176d
binary <bin4ryio@users.noreply.github.com>
2018-01-05 09:06:48 +0800

    Add example (#31)

---
+++ example/nuxt.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  modules: ["nuxtent"],
+  nuxtent: {
+    content: {
+      page: "/_post",
+      permalink: ":year/:slug",
+      generate: [
+        // assets to generate static build
+        "get",
+        "getAll"
+      ]
+    }
+  }
+};
